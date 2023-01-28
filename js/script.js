ee0729833169579c53cf1c0b047a01a5140cea27
Mr-Freedom-1 <glebpoterailo@gmail.com>
2023-01-29 00:12:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*-------------Завдання №1-------------*/
+// let h2 = document.querySelectorAll('h2');
+// h2[1].innerHTML = 'DOM method description';
+
+/*-------------Завдання №2-------------*/
+// console.log(document.querySelectorAll('[href^="site.ua/ua/"]'));
+
+/*-------------Завдання №3-------------*/
+// let list = document.querySelector('ul');
+// let li0 = document.createElement('li');
+// let li2 = document.createElement('li');
+// let li4 = document.createElement('li');
+// let li1 = document.querySelector('#first');
+// let li3 = document.querySelector('#third');
+// li0.setAttribute('id', 'null');
+// li2.setAttribute('id', 'second');
+// li4.setAttribute('id', 'fourth');
+// li0.innerHTML = 0;
+// list.prepend(li0);
+// li2.innerHTML = 2;
+// list.prepend(li2);
+// li4.innerHTML = 4;
+// list.prepend(li4);
+// li3.after(li4);
+// li1.after(li2);
+
+/*-------------Завдання №4-------------*/
+// let body = document.querySelector('body');
+// let h1 = document.querySelector('h1');
+// let a = document.createElement('a');
+// a.setAttribute('id', 'link');
+// a.setAttribute('href', 'https://dom.spec.whatwg.org/');
+// body.prepend(a);
+// a.append(h1);
